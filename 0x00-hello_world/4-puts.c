5c50cf2ae60112ccd4d45a90e5ccdf6a5857938c
Chimafrancis <chimafrancis75@gmail.com>
2022-12-09 13:07:58 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 /**
  *
@@ -6,8 +7,7 @@
  *   * Return: 0 if exited properly, non-zero otherwise
  *
  *    */
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0)
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
